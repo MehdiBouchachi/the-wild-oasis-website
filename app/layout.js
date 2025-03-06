20e80b027a6965983bf6557c0b9b3c10603d9e6f
MehdiBouchachi <mehdibch.dev@gmail.com>
2025-03-07 00:59:19 +0100

    building the about page  and cabins layout

--- app/layout.js
+++ app/layout.js
@@ -1,25 +1,31 @@
-import Logo from "@/app/_components/Logo";
-import Navigation from "@/app/_components/Navigation";
 import "@/app/_styles/globals.css";
+import Header from "@/app/_components/Header";
+import { Josefin_Sans } from "next/font/google";
 
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
 export const metadata = {
   //title: "The Wild Oasis", V1
   title: {
     template: "%s | The Wild Oasis",
     default: "Welcome | The Wild Oasis",
   },
-  description : "Luxurious cabin hotel, located in the heart of the Italian Dolomites, surrounded by the most beautiful mountains and the darkest forests in the world."
+  description:
+    "Luxurious cabin hotel, located in the heart of the Italian Dolomites, surrounded by the most beautiful mountains and the darkest forests in the world.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-primary-950 text-primary-100 min-h-screen">
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>{children}</main>
+      <body
+        className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}
+      >
+        <Header />
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl mx-auto">{children}</main>
+        </div>
         <footer>Copyrights by The Wild Oasis</footer>
       </body>
     </html>
