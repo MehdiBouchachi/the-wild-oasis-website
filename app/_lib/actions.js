87a40142461d4428fd60a77b26b5036daf6df9b7
MehdiBouchachi <mehdibch.dev@gmail.com>
2025-03-12 03:25:07 +0100

    completing the profile form page

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,5 +1,6 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
 import { auth, signIn, signOut } from "./auth";
 import { supabase } from "./supabase";
 
@@ -22,10 +23,9 @@ export async function updateGuest(formData) {
     .update(updateData)
     .eq("id", session.user.guestId);
 
-  if (error) {
-    console.error(error);
-    throw new Error("Guest could not be updated");
-  }
+  if (error) throw new Error("Guest could not be updated");
+
+  revalidatePath("/account/profile");
 }
 
 export async function signInAction() {
--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -143,7 +143,10 @@ export async function getCountries() {
       "https://restcountries.com/v2/all?fields=name,flag"
     );
     const countries = await res.json();
-    return countries;
+
+    return countries.filter(
+      (country) => country.name.toLowerCase() !== "israel"
+    );
   } catch {
     throw new Error("Could not fetch countries");
   }
