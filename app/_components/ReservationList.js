5f748cdc95ba46be5573020ce79a00cfac059ed5
MehdiBouchachi <mehdibch.dev@gmail.com>
2025-03-13 05:55:23 +0100

    Adding useOptimistic hook to reservationList

--- app/_components/ReservationList.js
+++ app/_components/ReservationList.js
@@ -1,8 +1,29 @@
+"use client";
+import { useOptimistic } from "react";
+import deleteReservation from "../_lib/actions";
+import ReservationCard from "./ReservationCard";
+
 function ReservationList({ bookings }) {
+  const [optimisticBookings, OptimisticDelete] = useOptimistic(
+    bookings,
+    (curBookings, bookingId) => {
+      return curBookings.filter((booking) => booking.id !== bookingId);
+    }
+  );
+
+  async function handleDelete(bookingId) {
+    OptimisticDelete(bookingId);
+    await deleteReservation(bookingId);
+  }
+
   return (
     <ul className="space-y-6">
-      {bookings.map((booking) => (
-        <ReservationCard booking={booking} key={booking.id} />
+      {optimisticBookings.map((booking) => (
+        <ReservationCard
+          onDelete={handleDelete}
+          booking={booking}
+          key={booking.id}
+        />
       ))}
     </ul>
   );
