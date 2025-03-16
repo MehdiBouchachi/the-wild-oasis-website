4a3fa1f5fcbccef2778c58d2c391c07aaf0b82b1
MehdiBouchachi <mehdibch.dev@gmail.com>
2025-03-16 10:30:08 +0100

    make the logo responsive

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -1,17 +1,19 @@
 import Image from "next/image";
 import Link from "next/link";
 import logo from "@/public/logo.png";
+
 function Logo() {
   return (
-    <Link href="/" className="flex items-center gap-4 z-10">
+    <Link href="/" className="flex items-center gap-3 sm:gap-4 z-10">
       <Image
         src={logo}
-        height="60"
-        width="60"
+        height={48} // slightly smaller on mobile
+        width={48}
         alt="The Wild Oasis logo"
         quality={100}
+        className="sm:h-[60px] sm:w-[60px] h-[48px] w-[48px]"
       />
-      <span className="text-xl font-semibold text-primary-100">
+      <span className="text-lg sm:text-xl font-semibold text-primary-100">
         The Wild Oasis
       </span>
     </Link>
